3903bffc0fc5cf3c1a61f5c297526cf8391fce23
杜震霖 <duzhenlin@vip.qq.com>
2020-02-13 12:53:38 +0800

    修改文档

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,10 @@ export default {
   data() {
     return {
       Signature: {
-        Policy:
-          "eyJleHBpcmF0aW9uIjoiMjAyMC0wMi0xMlQyMTozMjoyOS4yODhaIiwiY29uZGl0aW9ucyI6W3siYnVja2V0Ijoic2R5LXNkdHYifSxbInN0YXJ0cy13aXRoIiwiJGtleSIsIiJdLFsic3RhcnRzLXdpdGgiLCIkYWNsIiwiIl0sWyJzdGFydHMtd2l0aCIsIiRuYW1lIiwiIl0sWyJzdGFydHMtd2l0aCIsIiR4LWtzcy1tZXRhLWN1c3RvbS1wYXJhbTEiLCIiXSxbInN0YXJ0cy13aXRoIiwiJHgta3NzLW5ld2ZpbGVuYW1lLWluLWJvZHkiLCIiXSxbInN0YXJ0cy13aXRoIiwiJENhY2hlLUNvbnRyb2wiLCIiXSxbInN0YXJ0cy13aXRoIiwiJEV4cGlyZXMiLCIiXSxbInN0YXJ0cy13aXRoIiwiJENvbnRlbnQtRGlzcG9zaXRpb24iLCIiXSxbInN0YXJ0cy13aXRoIiwiJENvbnRlbnQtVHlwZSIsIiJdLFsic3RhcnRzLXdpdGgiLCIkQ29udGVudC1FbmNvZGluZyIsIiJdXX0=",
-        Signature: "cWJB2unUYe1Ip2tA7f8RdrNBEY0=",
-        KSSAccessKeyId: "AKLTcXGKGnyaSPSJ2qBryYGUvg",
-        bucket: "sdy-sdtv"
+        Policy: "",
+        Signature: "",
+        KSSAccessKeyId: "",
+        bucket: ""
       }
     };
   },
